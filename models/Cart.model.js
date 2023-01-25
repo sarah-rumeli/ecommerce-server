c97734a634b5301ae40f0484f448393384aa5dac
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2023-01-25 10:19:55 +0100

    Updated Cart model

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -3,7 +3,17 @@ const { Schema, model } = require("mongoose");
 const cartSchema = new Schema(
     {
       user: { type: Schema.Types.ObjectId, ref: 'User' },
-      product: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+      products: [{ 
+        productId: {type: Schema.Types.ObjectId, ref: 'Product'},
+        name: String,
+        quantity: {
+          type: Number,
+          required: true,
+          min: [1, 'Quantity can not be less than 1.'],
+          default: 1
+        }
+       }],
+       total: {type: Number, required: true, default: 0}
     },
     {
       // this second object adds extra properties: `createdAt` and `updatedAt`
