c60c7c26e045c8aff77385b1fd1a333f5676c781
Rumeli Paul <rumelipaul@gmail.com>
2023-01-30 20:58:00 +0100

    added delete cart route on checkout

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -22,6 +22,7 @@ router.get('/:user', isAuthenticated, (req, res, next) => {
     });
 });
 
+
 // POST /api/cart - Creates a new cart / updates users' cart
 router.post("/", (req, res, next) => {
   const {user, product} = req.body;
@@ -116,5 +117,16 @@ router.delete('/:productId', (req, res, next) => {
       })
       .catch(error => res.status(200).json(error));
 });
- 
+
+// DELETE /api/cart/remove/:userId - Removes Users' cart on checkout
+router.delete('/remove/:userId', (req, res, next) => {
+  const {userId}   = req.params;
+  
+    Cart.deleteOne({user: userId})
+      .then(() => res.json({ message: `Cart removed successfully.` }))
+      .catch(error => res.status(200).json(error));
+});
+
+
+
 module.exports = router;
